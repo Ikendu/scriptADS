22d9802f6dcca478606a7a1679f53f031f53e670
Chibundu <ndubest56@gmail.com>
2025-02-02 02:27:44 +0100

    linked List script

--- linkedList.js
+++ linkedList.js
@@ -56,9 +56,18 @@ class LinkedList {
       this.tail = newNode
     } else {
       newNode.next = this.head
-      this.head.next = newNode
+      this.head = newNode
     }
     this.length++
+    return this
+  }
+  shift() {
+    if (!this.head) return undefined
+    let temp = this.head
+    this.head = this.head.next
+    temp.next = null
+    this.length--
+    if (length == 0) this.tail = null
     return this
   }
 }
