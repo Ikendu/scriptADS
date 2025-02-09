ca00a070fdf47cd3f18cb1b2c096513d8e29a25f
Chibundu <ndubest56@gmail.com>
2025-02-09 01:45:38 +0100

    queue operations

--- 02-StackQueue/queue.js
+++ 02-StackQueue/queue.js
@@ -4,3 +4,41 @@ class Node {
     this.next = null
   }
 }
+
+class Queue {
+  constructor(value) {
+    let newNode = new Node(value)
+    this.first = newNode
+    this.last = newNode
+    this.length++
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value)
+    if (!this.first) {
+      this.first = newNode
+      this.last = newNode
+    }
+    //   let temp = this.last
+    this.last.next = newNode
+    this.last = newNode
+
+    return this
+  }
+
+  dequeue() {
+    if (!this.top) return undefined
+    let temp = this.top
+
+    this.top = this.top.next
+    temp.next = null
+    this.length--
+
+    return temp
+  }
+}
+
+let myQueue = new Queue(1)
+myQueue.enqueue(3)
+myQueue.enqueue(5)
+console.log(myQueue)
