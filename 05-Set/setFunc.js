49892347df9a72ab4cc1d72a4e58f9e591d942f2
Chibundu <ndubest56@gmail.com>
2025-02-15 06:15:58 +0100

    longest consecutive string

--- 05-Set/setFunc.js
+++ 05-Set/setFunc.js
@@ -44,3 +44,25 @@ function findPairs(arr1, arr2, num) {
 }
 
 console.log(findPairs([1, 2, 3], [4, 5, 6], 7))
+
+function longestConsecutiveString(arr) {
+  let mySet = new Set(arr)
+  let totalSum = 0
+
+  for (let num of mySet) {
+    // if (!mySet.has(num - 1)) {
+    let currNum = num
+    let sum = 1
+
+    while (mySet.has(currNum + 1)) {
+      currNum++
+      sum++
+    }
+    totalSum = Math.max(sum, totalSum)
+    // }
+  }
+  console.log(mySet)
+
+  return totalSum
+}
+console.log(longestConsecutiveString([9, 1, 3, 10, 10, 2, 20, 3, 21]))
