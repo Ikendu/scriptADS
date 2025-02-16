1d5d8ab01f369e182e5656cbe7c45b393d2fd3ab
Chibundu <ndubest56@gmail.com>
2025-02-16 16:22:25 +0100

    more on remove edge

--- graph.js
+++ graph.js
@@ -10,11 +10,38 @@ class Graph {
     }
     return false
   }
+  addEdge(v1, v2) {
+    if (this.obj[v1] && this.obj[v2]) {
+      if (this.obj[v1].indexOf(v2) == -1) {
+        this.obj[v1].push(v2)
+        this.obj[v2].push(v1)
+        return true
+      }
+      return false
+    }
+
+    return this
+  }
+  removeEdge(v1, v2) {
+    if (this.obj[v1] && this.obj[v2]) {
+      let index1 = this.obj[v1].indexOf(v2)
+      let index2 = this.obj[v2].indexOf(v1)
+      if (index1 > -1 && index2 > -1) {
+        this.obj[v1].splice(index1, 1)
+        this.obj[v2].splice(index2, 1)
+        return true
+      }
+      return false
+    }
+  }
 }
 
 let mygraph = new Graph()
 mygraph.addVertice('A')
 mygraph.addVertice('B')
 mygraph.addVertice('C')
-console.log(mygraph.addVertice('C'))
+mygraph.addEdge('A', 'B')
+mygraph.addEdge('A', 'B')
+// mygraph.removeEdge('A', 'B')
+// console.log(mygraph.addVertice('C'))
 console.log(mygraph)
