404c0fecce00e5ef81f70a9c394833d9f82dc293
Chibundu <ndubest56@gmail.com>
2025-02-24 05:10:23 +0100

    max sub array

--- arrayQuestion/maxSubArray.js
+++ arrayQuestion/maxSubArray.js
@@ -1,18 +1,18 @@
 function maxSum(arr) {
   if (arr.length == 0) return 0
 
-  let subArr = arr[0]
-  let maxSub = arr[0]
+  let subSum = arr[0]
+  let maxSum = arr[0]
 
   for (let i = 1; i < arr.length; i++) {
-    subArr = Math.max(subArr + arr[i], arr[i])
-    maxSub = Math.max(subArr, maxSub)
+    subSum = Math.max(subSum + arr[i], arr[i])
+    maxSum = Math.max(subSum, maxSum)
   }
-  return maxSub
+  return maxSum
 }
 
 console.log(maxSum([1, 2, 3, 4]))
 console.log(maxSum([-1, -2, -3, -4]))
-console.log(maxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+console.log(maxSum([-2, 1, -3, 4, -1, 2, 1, 10, -5, 4]))
 console.log(maxSum([-1, -2, -3, 0, 0]))
 // console.log(maxSum([]))
