ec3ded8d307e84e8bc5e1a6a860acbd5b95532e2
Chibundu <ndubest56@gmail.com>
2025-02-25 21:36:28 +0100

    stack class

---
+++ OTHERS/stack.js
@@ -0,0 +1,32 @@
+let Stack = function () {
+  this.store = {}
+  this.length = 0
+
+  this.push = (value) => {
+    this.store[this.length] = value
+    this.length++
+  }
+
+  this.pop = () => {
+    this.length--
+    let result = this.store[this.length]
+    delete this.store[this.length]
+    return result
+  }
+
+  this.peek = () => {
+    return this.store[this.length - 1]
+  }
+
+  this.items = () => {
+    return this.store
+  }
+}
+
+let myStack = new Stack()
+myStack.push(10)
+myStack.push(20)
+myStack.push(30)
+myStack.pop()
+console.log(myStack.peek())
+console.log(myStack)
