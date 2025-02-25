4d87dc13739a995c90a6d3b7286c1a9bd7fedd1d
Chibundu <ndubest56@gmail.com>
2025-02-25 22:47:56 +0100

    more on set

--- OTHERS/set.js
+++ OTHERS/set.js
@@ -19,6 +19,10 @@ function MySet() {
     }
   }
 
+  this.items = () => {
+    return this.values
+  }
+
   this.union = (otherSet) => {
     let union = new MySet()
     let myset = this.values
@@ -34,20 +38,36 @@ function MySet() {
     return union
   }
 
-  this.items = () => {
-    return this.values
+  this.intersection = (otherSet) => {
+    let intersect = new MySet()
+
+    this.values.forEach((e) => {
+      otherSet.has(e) && intersect.add(e)
+    })
+    return intersect
+  }
+
+  this.difference = (otherSet) => {
+    let diff = new MySet()
+
+    this.values.forEach((e) => {
+      !otherSet.has(e) && diff.add(e)
+    })
+    return diff
   }
 }
 
 let setA = new MySet()
 setA.add(10)
-setA.add(10)
+setA.add(50)
 setA.add(20)
 
 let setB = new MySet()
 setB.add(30)
 setB.add(40)
-setB.add(50)
+setB.add(10)
 console.log(setA.has(10))
 console.log(setA.items())
 console.log(setA.union(setB).items())
+console.log(setA.intersection(setB).items())
+console.log(setA.difference(setB).items())
