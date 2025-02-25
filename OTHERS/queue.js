bb755bd8df8c8981f04cfb2a690346fa49b5a080
Chibundu <ndubest56@gmail.com>
2025-02-25 23:25:54 +0100

    enqueue

--- OTHERS/queue.js
+++ OTHERS/queue.js
@@ -16,10 +16,33 @@ function Queue() {
   this.items = function () {}
 }
 
-let myQueue = new Queue()
-myQueue.add(10)
-myQueue.add(20)
-myQueue.add(40)
-console.log(myQueue.store)
-console.log(myQueue.peek())
-console.log(myQueue.length)
+// let myQueue = new Queue()
+// myQueue.add(10)
+// myQueue.add(20)
+// myQueue.add(40)
+// console.log(myQueue.store)
+// console.log(myQueue.peek())
+// console.log(myQueue.length)
+
+function PriorityQueue() {
+  this.store = []
+
+  this.enqueue = function (element) {
+    if (!this.store.length) {
+      this.store.push(element)
+    }
+    let checker = 0
+
+    for (let i = 0; i < this.store.length; i++) {
+      if (element[1] < this.store[i][1]) {
+        this.store.splice(i, 0, element)
+        checker = 1
+      }
+    }
+    if (checker == 0) {
+      this.store.push(element)
+    }
+    }
+    
+    
+}
