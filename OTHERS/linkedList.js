85b8b33ae037733479a930341714f9c3778396b5
Chibundu <ndubest56@gmail.com>
2025-03-01 13:10:37 +0100

    insert into list

--- OTHERS/linkedList.js
+++ OTHERS/linkedList.js
@@ -1,4 +1,4 @@
-function List() {
+function LinkedList() {
   let length = 0
   let head = null
 
@@ -7,11 +7,19 @@ function List() {
     this.next = null
   }
 
-  function isEmpty() {
-    return this.length == 0
+  this.isEmpty = function () {
+    return length == 0
+  }
+  this.show = function () {
+    let values = []
+    while (head) {
+      values.push(head.value)
+      head = head.next
+    }
+    return values
   }
 
-  function addNode(element) {
+  this.addNode = function (element) {
     let newNode = new Node(element)
 
     if (head == null) {
@@ -24,6 +32,38 @@ function List() {
       }
       currNode.next = newNode
     }
+    length++
     return this
   }
+
+  this.insert = function (element, index) {
+    if (index > length) return false
+
+    let newNode = new Node(element)
+    let curr = head
+    let prev
+    let pos = 0
+
+    if (index == 0) {
+      newNode.next = curr
+      head = newNode
+    } else {
+      while (pos < index) {
+        pos++
+        prev = curr
+        curr = curr.next
+      }
+      newNode.next = curr
+      prev.next = newNode
+    }
+    length++
+  }
 }
+
+let myList = new LinkedList()
+myList.addNode(9)
+myList.addNode(19)
+myList.addNode(10)
+myList.insert('Hello', 1)
+console.log(myList.isEmpty())
+console.log(myList.show())
