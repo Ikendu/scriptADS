73341c317a17603e63c25d8dbad6fc883564e54c
Chibundu <ndubest56@gmail.com>
2025-03-20 11:55:20 +0100

    minimum cost climbing

--- dynamicProgramming/minimumCostClimbing.js
+++ dynamicProgramming/minimumCostClimbing.js
@@ -13,3 +13,16 @@ function minimumCostClimbing(costs) {
 
 console.log(minimumCostClimbing([10, 15, 20]))
 console.log(minimumCostClimbing([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+
+function minCostClimb(costs) {
+  costs.push(0)
+
+  for (let i = costs.length - 4; i >= 0; i--) {
+    costs[i] += Math.min(costs[i + 2], costs[i + 1])
+  }
+
+  return Math.min(costs[0], costs[1])
+}
+
+console.log(minCostClimb([10, 15, 20]))
+console.log(minCostClimb([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
