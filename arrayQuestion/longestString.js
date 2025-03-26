ba7a70ea96538e8b4acc2971322ca5483be28a38
Chibundu <ndubest56@gmail.com>
2025-03-26 11:31:28 +0100

    more on longest string

--- arrayQuestion/longestString.js
+++ arrayQuestion/longestString.js
@@ -8,5 +8,25 @@ function longestString(arr) {
   }
   return longest
 }
-
 console.log(longestString(['orag', 'cherr', 'banana']))
+
+// Wrong
+// function longest(arr) {
+//   let longest = ''
+//   for (let str of arr) {
+//     longest = Math.max(longest.length, str.length)
+//   }
+//   return longest
+// }
+
+console.log(longest(['orag', 'cherr', 'banana']))
+
+function longestStr(arr) {
+  let longest = ''
+  for (str of arr) {
+    longest = longest.length > str.length ? longest : str
+  }
+  return longest
+}
+
+console.log(longestStr(['orag', 'cherr', 'banana', 'tomatoes']))
