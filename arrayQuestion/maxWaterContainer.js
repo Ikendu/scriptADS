e30b885a1c542d765990c2012c43e68ac4008404
Chibundu <ndubest56@gmail.com>
2025-03-27 16:00:11 +0100

    more on ads

--- arrayQuestion/maxWaterContainer.js
+++ arrayQuestion/maxWaterContainer.js
@@ -1,17 +1,36 @@
-function maxContainer(container) {
+// function maxContainer(container) {
+//   let left = 0
+//   let right = container.length - 1
+//   let area = 0
+
+//   while (left < right) {
+//     let curr = (right - left) * Math.min(container[left], container[right])
+//     area = Math.max(area, curr)
+
+//     if (container[left] < container[right]) left++
+//     else right--
+//   }
+
+//   return area
+// }
+
+function maxContain(areas) {
   let left = 0
-  let right = container.length - 1
-  let area = 0
+  let right = areas.length - 1
+  let result = 0
 
   while (left < right) {
-    let curr = (right - left) * Math.min(container[left], container[right])
-    area = Math.max(area, curr)
+    let cur = (right - left) * Math.min(areas[left], areas[right])
+    result = Math.max(cur, result)
 
-    if (container[left] < container[right]) left++
-    else right--
+    if (areas[left] < areas[right]) {
+      left++
+    } else {
+      right--
+    }
   }
 
-  return area
+  return result
 }
-console.log(maxContainer([1, 8, 6, 2, 5, 4, 8, 3, 7]))
-console.log(maxContainer([1, 1]))
+console.log(maxContain([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+console.log(maxContain([1, 1]))
