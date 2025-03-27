310ddf08f4b51e9aaaeea12ad01f8c4c8e42a9a2
Chibundu <ndubest56@gmail.com>
2025-03-27 18:37:55 +0100

    no revice nodup

--- arrayQuestion/noDuplicate.js
+++ arrayQuestion/noDuplicate.js
@@ -12,16 +12,26 @@ function noduplicate(arr) {
 console.log(noduplicate([4, 5, 29, 'hello', 'hello', 29, 4]))
 
 // For Sorted Array
+// return the number of duplicate
 function nodup(arr) {
-  let count = 1
+  let count = 0
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] !== arr[i - 1]) {
       arr[count] = arr[i]
       count++
     }
   }
-  //   console.log(arr.slice(0, count))
   return count
+}
+
+console.log(nodup([4, 4, 5, 29, 29, 'hello', 'hello']))
+
+// count the number of duplicate
+
+function nodup1(arr) {
+  let myset = new Set(arr)
+
+  return arr.length - myset.size
 }
 
 console.log(nodup([4, 4, 5, 29, 29, 'hello', 'hello']))
